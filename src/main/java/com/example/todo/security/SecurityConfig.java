3f9a03a80a8beb6698aafe66b07d85faec7a03c9
YJ0422 <24708595l30@gmail.com>
2026-02-06 01:13:12 +0900

    modified access permission for Swagger

--- src/main/java/com/example/todo/security/SecurityConfig.java
+++ src/main/java/com/example/todo/security/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig {
     private static final String[] SWAGGER_ACCESSPOINT = {
             "/swagger-ui/**",
             "/swagger-ui.html",
+            "/v3/api-docs",
             "/v3/api-docs/**",
             "/v3/api-docs.yaml",
             "/webjars/**"
